15a5d2b579889bf44729ec1472b3b5e8da39d12b
Andrea Parisi <andreaparisi4502@gmail.com>
2021-12-01 12:40:31 +0100

    added api and title search

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,28 +3,40 @@
     <h1>BoolFix</h1>
     <input type="text" v-model="title">
     <button @click="search" @keydown.enter="search()">Search</button>
+      {{this.apiSearch}}
     <ul>
-      <li>{{this.apiSearch}}</li>
+      <li v-for="film,i in films" :key="i">{{film}}</li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios"
 export default {
   name: 'Main',
   data(){
     return{
       title:"",
       apiSearch:"",
-      apiUrl:"https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&language=it-IT&="
+      films:[],
+      apiUrl:"https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&language=it-IT&query="
     }
   },
   methods: {
     search(){
-      this.title.replace(/\s/g, '+');
-      console.log(this.title);
       let apiSearch = this.apiUrl + this.title;
       this.apiSearch = apiSearch;
+      axios
+      .get(this.apiSearch)
+      .then((result)=>{
+        let risultato = result.data.results;
+        let tempTitle = [];
+
+        for(let i = 0; i < risultato.length ; i++){
+          tempTitle.push(risultato[i].title)
+        }
+        this.films = tempTitle;
+      });
     }
   }
 }
