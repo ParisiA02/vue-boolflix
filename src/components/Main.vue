47c95fafa41b5d4f1092ecbf1f232cd365df50ea
Andrea Parisi <andreaparisi4502@gmail.com>
2021-12-02 15:27:18 +0100

    add vote and style

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,113 +3,118 @@
     <h1>BoolFix</h1>
     <input type="text" v-model="title" @keydown.enter="search">
     <button @click="search">Search</button>
-    <ul v-for="film,k in films" :key="'A' + k">
-      <li>{{film.title}}</li>
-      <li>{{film.original_title}}</li>
-      <li v-if="film.backdrop_path !== null"> <img :src="filmImgUrl + film.backdrop_path" alt="img"></li>
-      <li v-else>IMMAGINE ASSENTE</li>
-      <li v-if="film.original_language === 'en'"> <img src="../assets/inghilterra.png" alt="en-flag"> </li>
-      <li v-else-if="film.original_language === 'it'"> <img src="../assets/italia.png" alt="ita-flag"> </li>
-      <li v-else-if="film.original_language === 'es'"> <img src="../assets/spagna.png" alt="es-flag"> </li>
-      <li v-else-if="film.original_language === 'fr'"> <img src="../assets/francia.png" alt="fr-flag"> </li>
-      <li v-else>{{film.original_language}}</li>
-      <li v-if="votes[k] === 0">
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>      
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[k] === 1">
-        <i class="fas fa-star"></i>
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>      
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[k] === 2">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="far fa-star"></i>      
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[k] === 3">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>      
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[k] === 4">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>      
-        <i class="fas fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[k] === 5">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>      
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-      </li>
-
-    </ul>
-    <ul v-for="serie,i in tvSeries" :key="'B' + i">
-      <li>{{serie.name}}</li>
-      <li>{{serie.original_name}}</li>
-      <li v-if="serie.backdrop_path !== null"> <img :src="filmImgUrl + serie.backdrop_path" alt="img"></li>
-      <li v-else>IMMAGINE ASSENTE</li>
-      <li v-if="serie.original_language === 'en'"> <img src="../assets/inghilterra.png" alt="en-flag"> </li>
-      <li v-else-if="serie.original_language === 'it'"> <img src="../assets/italia.png" alt="ita-flag"> </li>
-      <li v-else-if="serie.original_language === 'es'"> <img src="../assets/spagna.png" alt="es-flag"> </li>
-      <li v-else-if="serie.original_language === 'fr'"> <img src="../assets/francia.png" alt="fr-flag"> </li>
-      <li v-else>{{serie.original_language}}</li>
-      <li v-if="votes[i] === 0">
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>      
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[i] === 1">
-        <i class="fas fa-star"></i>
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>      
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[i] === 2">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="far fa-star"></i>      
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[i] === 3">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>      
-        <i class="far fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[i] === 4">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>      
-        <i class="fas fa-star"></i>
-        <i class="far fa-star"></i>
-      </li>
-      <li v-else-if="votes[i] === 5">
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>      
-        <i class="fas fa-star"></i>
-        <i class="fas fa-star"></i>
-      </li>
-    </ul>
+    <div class="card-container">
+        <div class="card" v-for="film,k in films" :key="'A' + k">
+          <div class="img-container" v-if="film.backdrop_path !== null"> <img :src="filmImgUrl + film.backdrop_path" alt="img"></div>
+          <div v-else> <img class="default-img" src="../assets/film.png" alt="default-film-img"></div>
+          <ul>
+            <li>Titolo: {{film.title}}</li>
+            <li>Titolo Originale: {{film.original_title}}</li> 
+            <li v-if="film.original_language === 'en'"> Lingua: <img class="img-lingua" src="../assets/inghilterra.png" alt="en-flag"> </li>
+            <li v-else-if="film.original_language === 'it'"> Lingua: <img class="img-lingua" src="../assets/italia.png" alt="ita-flag"> </li>
+            <li v-else-if="film.original_language === 'es'"> Lingua: <img class="img-lingua" src="../assets/spagna.png" alt="es-flag"> </li>
+            <li v-else-if="film.original_language === 'fr'"> Lingua: <img class="img-lingua" src="../assets/francia.png" alt="fr-flag"> </li>
+            <li v-else>Lingua: {{film.original_language}}</li>
+            <li v-if="votes[k] === 0">
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[k] === 1">
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[k] === 2">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[k] === 3">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[k] === 4">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[k] === 5">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </li>
+          </ul>
+        </div>
+        <div class="card" v-for="serie,i in tvSeries" :key="'B' + i">
+          <div class="img-container" v-if="serie.backdrop_path !== null"> <img :src="filmImgUrl + serie.backdrop_path" alt="img"></div>
+          <div v-else> <img class="default-img" src="../assets/tvserie.png" alt="default-serie-img"></div>
+          <ul>
+            <li>Titolo: {{serie.name}}</li>
+            <li>Titolo Originale: {{serie.original_name}}</li>
+            <li v-if="serie.original_language === 'en'"> Lingua: <img class="img-lingua" src="../assets/inghilterra.png" alt="en-flag"> </li>
+            <li v-else-if="serie.original_language === 'it'"> Lingua: <img class="img-lingua" src="../assets/italia.png" alt="ita-flag"> </li>
+            <li v-else-if="serie.original_language === 'es'"> Lingua: <img class="img-lingua" src="../assets/spagna.png" alt="es-flag"> </li>
+            <li v-else-if="serie.original_language === 'fr'"> Lingua: <img class="img-lingua" src="../assets/francia.png" alt="fr-flag"> </li>
+            <li v-else>Lingua: {{serie.original_language}}</li>
+            <li v-if="votes[i] === 0">
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[i] === 1">
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[i] === 2">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[i] === 3">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[i] === 4">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+            </li>
+            <li v-else-if="votes[i] === 5">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </li>
+          </ul>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -126,7 +131,7 @@ export default {
       seriesApiUrl:"https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=",
       films:[],
       tvSeries:[],
-      filmImgUrl:"https://image.tmdb.org/t/p/w300",
+      filmImgUrl:"https://image.tmdb.org/t/p/w342",
       votes: []
     }
   },
@@ -166,8 +171,65 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
-ul {
-  list-style-type: none;
-  padding: 0;
+@import '~@fortawesome/fontawesome-free/css/all.min.css';
+
+.container{
+  width: 100%;
+  min-height: 100vh;
+  background-color: rgb(54, 50, 50);
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  .card-container{
+    width: 100%;
+    display:  flex;
+    flex-wrap: wrap;
+    margin: 0 auto;
+  
+    .default-img{
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  
+    .card{
+      width: calc(100%/6);
+      height: 193px;
+      position: relative;
+      border: 1px solid white;
+  
+      &:hover{
+        background-color: black;
+        img{
+          display: none;
+        }
+
+        img.img-lingua{
+          display: inline-block;
+          width: 20px;
+        }
+
+        ul li{
+          display: block;
+          margin: 10px 0 0 10px;
+        }
+      }
+  
+      img{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+      }
+      
+      ul li{
+        position: relative;
+        z-index: 2;
+        color: white;
+        display: none;
+      }
+    }
+  }
 }
 </style>
